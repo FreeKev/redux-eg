e9b0bb3734dc932952f349ee1b95993b03a5c23e
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-05-16 11:19:38 -0700

    Post payload route

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/postActions';
 
 class PostForm extends React.Component {
   constructor(props){
@@ -23,15 +26,8 @@ class PostForm extends React.Component {
       body: this.state.body
     }
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-      .then(res => res.json())
-      .then(data => console.log(data));
+    // Call action
+    this.props.createPost(post);
   }
 
   render() {
@@ -60,4 +56,8 @@ class PostForm extends React.Component {
   }
 }
 
-export default PostForm;
+PostForm.propTypes = {
+  createPost: PropTypes.func.isRequired
+};
+
+export default connect(null, { createPost })(PostForm);

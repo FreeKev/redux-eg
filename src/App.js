4b4248fd9c0881935630c63ba60233ba953267cf
Joe Johnson <joe@Joes-MacBook-Pro.local>
2018-05-16 10:53:18 -0700

    Redux boiler, up.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/Postform';
+import store from './store'; 
+
+// get redux working with empty input
+// const store = createStore(() => [], {}, applyMiddleware());
 
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -16,6 +22,7 @@ class App extends Component {
         <PostForm />
         <Posts />
       </div>
+      </Provider>
     );
   }
 }
